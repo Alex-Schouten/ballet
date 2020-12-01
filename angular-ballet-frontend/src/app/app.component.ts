bd7e7008679c8b137c89a826b718f897d5add349
Alex-Schouten <alexandraschouten@hotmail.com>
2020-12-01 14:18:41 +0100

    first commit

---
+++ angular-ballet-frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-ballet';
+  footer = 'A performance brought to you by Alex Schouten | 2020 | All rights reserved';
+
+}
---
+++ angular-ballet-frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TicketsComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
